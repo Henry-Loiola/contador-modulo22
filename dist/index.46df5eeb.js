265e357102ac8c3a9baf000e96db430b08df3185
Henry Loiola <henryloiola@gmail.com>
2024-01-05 02:08:53 -0300

    first commit

---
+++ dist/index.46df5eeb.js
@@ -0,0 +1,2 @@
+AOS.init();const e=new Date("Feb 1, 2024 23:59:59").getTime(),t=setInterval(function(){let n=e-new Date().getTime();document.getElementById("contador").innerHTML=`${Math.floor(n/864e5)}d ${Math.floor(n%864e5/36e5)}h ${Math.floor(n%36e5/6e4)}m ${Math.floor(n%6e4/1e3)}s`,n<0&&(clearInterval(t),document.getElementById("contador").innerHTML="Evento expirado")},1e3);
+//# sourceMappingURL=index.46df5eeb.js.map
